567d1e718fa8acf8679fd12603a6b61054e30c69
Kiarash Taherkhani <lxboxl@yahoo.com>
2020-10-18 11:58:56 +0330

    COMMIT

---
+++ PhobiaGameJam/Assets/Scripts/StayInside.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInside : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -6f, 6f),
+            Mathf.Clamp(transform.position.y, -4f, 0f), transform.position.z);
+	}
+}
